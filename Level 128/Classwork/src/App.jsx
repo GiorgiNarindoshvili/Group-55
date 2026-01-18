13b22f56374c10c1a92d6cacdae679bf8dbe62df
GiorgiNarindoshvili <giorgig20099@gmail.com>
2026-01-17 22:53:13 -0500

    commit

--- Level 128/Classwork/src/App.jsx
+++ Level 128/Classwork/src/App.jsx
@@ -1,11 +1,7 @@
 import { useState } from 'react'
 
 const App = () => {
- return(
-  <div>
-    <p className="text-yellow-300 justify-center flex">Hi</p>
-  </div>
- )
+ //No questions
 }
 
 export default App
